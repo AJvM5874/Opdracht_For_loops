88d89ca4eaac81d64dadca63bc2ca65750636fee
AJvM5874 <67021280+AJvM5874@users.noreply.github.com>
2021-06-06 22:11:42 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,10 @@
+let number = 0;
+while (number < 20) {
+    if (number % 2 == 0) {
+        console.log(number + " is even")
+    } else {
+        console.log(number + " is oneven")
+    }
+    number++
+
+}
